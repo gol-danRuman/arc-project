1f794aa539e0cd2a77dd9bded8e7c7748fc1757d
rumancha <rumancha12@gmail.com>
2020-08-16 22:50:37 +0545

    Updates

---
+++ precache-manifest.e612f110e064ab22b509ecd97e7469e2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d31a12e2cb7ad738da12ab3d4e31b62",
+    "url": "/arc-project/index.html"
+  },
+  {
+    "revision": "89dff089cc6cdb97b810",
+    "url": "/arc-project/static/css/2.fc81494e.chunk.css"
+  },
+  {
+    "revision": "d3a296f0b9f14804ac7a",
+    "url": "/arc-project/static/css/main.030330ce.chunk.css"
+  },
+  {
+    "revision": "89dff089cc6cdb97b810",
+    "url": "/arc-project/static/js/2.c6c7d42f.chunk.js"
+  },
+  {
+    "revision": "ecf022debae39227c670690ff85c1d18",
+    "url": "/arc-project/static/js/2.c6c7d42f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3a296f0b9f14804ac7a",
+    "url": "/arc-project/static/js/main.82e5422e.chunk.js"
+  },
+  {
+    "revision": "2b599280affe37b8365e",
+    "url": "/arc-project/static/js/runtime-main.4d690b38.js"
+  },
+  {
+    "revision": "3670aa493ee09e92c7b8e1e7c2f5b441",
+    "url": "/arc-project/static/media/CAVIARDREAMS.3670aa49.TTF"
+  },
+  {
+    "revision": "65ab651cc7cebf7331a4228e830573d1",
+    "url": "/arc-project/static/media/CAVIARDREAMS_BOLD.65ab651c.TTF"
+  },
+  {
+    "revision": "825e49e42ab834f96cf27fc09c82910d",
+    "url": "/arc-project/static/media/CAVIARDREAMS_BOLDITALIC.825e49e4.TTF"
+  },
+  {
+    "revision": "e67f5c38635c8f06c286b0c33da927bb",
+    "url": "/arc-project/static/media/CAVIARDREAMS_ITALIC.e67f5c38.TTF"
+  },
+  {
+    "revision": "7e647476cff3a37a632c862fc0a5b9b2",
+    "url": "/arc-project/static/media/c1.7e647476.jpg"
+  },
+  {
+    "revision": "9026f730ed03f9e3f0263943f9fdc3f5",
+    "url": "/arc-project/static/media/team.9026f730.jpg"
+  }
+]);
